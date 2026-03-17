c5e16bdc4e7f8d3ddcbff122f1b99d48f3a3289f
Vickouma77 <Vickprestone20@gmail.com>
2026-03-17 10:31:15 +0300

    Add SnippetView test with various ID scenarios; enhance assertion utility

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -18,3 +18,63 @@ func TestPing(t *testing.T) {
 	assert.Equal(t, code, http.StatusOK)
 	assert.Equal(t, body, "OK")
 }
+
+func TestSnippetView(t *testing.T) {
+	//New test application
+	app := newTestApplication(t)
+
+	//Test server
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	tests := []struct {
+		name     string
+		urlPath  string
+		wantCode int
+		wantBody string
+	}{
+		{
+			name:     "Valid ID",
+			urlPath:  "/stash/view/1",
+			wantCode: http.StatusOK,
+			wantBody: "An old silent pod...",
+		},
+		{
+			name:     "Non-existent ID",
+			urlPath:  "/stash/view/2",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Negative ID",
+			urlPath:  "/stash/view/-1",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Decimal ID",
+			urlPath:  "/stash/view/1.23",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "String ID",
+			urlPath:  "/stash/view/foo",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Empty ID",
+			urlPath:  "/stash/view/",
+			wantCode: http.StatusNotFound,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			code, _, body := ts.get(t, tt.urlPath)
+
+			assert.Equal(t, code, tt.wantCode)
+
+			if tt.wantBody != "" {
+				assert.StringContains(t, body, tt.wantBody)
+			}
+		})
+	}
+}
